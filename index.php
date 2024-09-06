2252ec8da27e905b5ee645a87dadc1b67eadbc06
Samuel Oliveira <samuel.silva@fixhealth.com.br>
2024-09-06 10:56:41 -0300

    teste 1233

--- index.php
+++ index.php
@@ -7,9 +7,6 @@
 </head>
 <body>
     <h1>Upload de Arquivo para OneDrive</h1>
-    <h1>Upload de Arquivo para OneDrive</h1>
-    <h1>Upload de Arquivo para OneDrive</h1>
-    <h1>commit</h1>
     <form action="src/Update.php" method="post" enctype="multipart/form-data">
         <label for="file">Escolha um arquivo para enviar:</label>
         <input type="file" name="file" id="file" required>
